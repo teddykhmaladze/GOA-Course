d03ef78803facd06f46c867e9c86576f304b4c7d
nikolozkakabadze123 <Nikakakabadze98@gmail.com>
2024-09-26 22:40:16 +0400

    commit

---
+++ Day 03/homework/task4.py
@@ -0,0 +1,7 @@
+#დაუმატეთ ერთმანეთს თქვენი 2 საყვარელი ფილმის სახელი
+
+movie1=Astrid_lindgren
+
+movie2=garbage_tree
+
+print(movie1 + movie2)
---
+++ Day 03/homework/task7.py
@@ -0,0 +1,20 @@
+#შემოიტანეთ 10 ცვლადი სადაც შეინახავთ ტექტს და ბრჭყალებიან რიცხვებს
+word1="hello"
+
+word2="big"
+
+word3="dog"
+
+word4="milk"
+
+word5="exit"
+
+word6="click"
+
+word7="from"
+
+word8="san"
+
+word9="ink"
+
+word10="forest"
