f412535c754b494d3a032863b76f593c3c87648c
teddykhmaladze <teddy.khmaladze28@gmail.com>
2025-04-11 18:12:54 +0400

    “commit“

---
+++ Level 31/classwork/lesson.py
@@ -0,0 +1,26 @@
+# 1.
+def four_numbers_sum(n1, n2, n3, n4):
+    print(n1+n2+n3+n4)
+four_numbers_sum(1,2,3,4)
+
+# 2.
+def func1(world,time):
+    print(world*time)
+func1("word",123)
+
+# 3.
+def calculator(num1,num2,operator):
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "/":
+        print(num1 / num2)
+    else:print(num1 * num2)
+calculator(1,"+",2)
+
+# 4.
+def func2(price, fasdakleba, money):
+    if money == 20:
+        print(price - fasdakleba)
+func2(1,1,1)
