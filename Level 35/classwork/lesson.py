df3040e0e5a53a1758da0ec4cd56925f83c58c3f
teddykhmaladze <teddy.khmaladze28@gmail.com>
2025-05-20 17:27:58 +0400

    commit

---
+++ Level 35/classwork/lesson.py
@@ -0,0 +1,21 @@
+names = ["Theodore", "Luka", "Andria"]
+names.insert(2, "Gurami")
+print(names)
+
+
+numbers = [1, 2, 3]
+numbers.append(5)
+numbers.append(7)
+numbers.append(9)
+numbers.append(10)
+print(numbers)
+
+
+numbers_list = [4, 5, 6, 7, 8, 9]
+numbers_list.pop(3)
+numbers_list.pop(4)
+print(numbers_list)
+
+
+numbers_list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(len(numbers_list2) / 5)
